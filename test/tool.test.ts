e091ad24d73d5b9471ed58c908c179076694bede
曾代段秋 <zengdaiduanqiu1@yupaowang.com>
2025-07-04 09:55:21 +0800

    build: 移除测试覆盖率报告相关文件
    - 删除了 clover.xml、coverage-final.json、base.css、block-navigation.js、favicon.png 和 index.html 等测试覆盖率报告相关文件
    - 这些文件通常由测试工具自动生成，不应当手动提交到版本控制系统中

--- test/tool.test.ts
+++ test/tool.test.ts
@@ -10,6 +10,6 @@ test.each([
   ["key", { prefix: "XXX " }, "XXX_key"],
   ["key", { prefix: " XXX " }, "XXX_key"],
   ["key", { prefix: "XXX_DEV_680" }, "XXX_DEV_680_key"],
-] as Array<[string, Config, string]>)(`test tool getFullPrefixKey(%s,%o) => %s`, (a, b, res) => {
+] as Array<[string, IMSLocalStorageConfig, string]>)(`test tool getFullPrefixKey(%s,%o) => %s`, (a, b, res) => {
   expect(getFullKeyStr(a, b)).toBe(res);
 });
